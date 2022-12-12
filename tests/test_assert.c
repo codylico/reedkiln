5b39da6f4c5cb309fc7bbee93db23d366fd75f37
Cody Licorish <svgmovement@gmail.com>
2022-12-11 19:30:24 -0500

    Move assert tests to assert suite

--- tests/test_assert.c
+++ tests/test_assert.c
@@ -7,11 +7,15 @@
 
 int test_assert(void*);
 int test_assert_fail(void*);
+int test_explicit_false(void*);
+int test_explicit_true(void*);
 int test_zeta(void*);
 
 struct reedkiln_entry tests[] = {
   { "assert", test_assert },
   { "assert_fail", test_assert_fail, Reedkiln_TODO },
+  { "explicit_false", test_explicit_false, Reedkiln_TODO },
+  { "explicit_true", test_explicit_true },
   { "zeta", test_zeta },
   { NULL, NULL }
 };
@@ -28,6 +32,23 @@ int test_assert_fail(void* p) {
   unsigned int v = reedkiln_rand();
   unsigned int w = reedkiln_rand();
   reedkiln_assert(v == w);
+  return Reedkiln_OK;
+}
+
+/* test for assert conversion to Boolean false */
+int test_explicit_false(void* p) {
+  reedkiln_assert(NULL);
+  return Reedkiln_OK;
+}
+
+/* test for assert conversion to Boolean true */
+int test_explicit_true(void* p) {
+  reedkiln_assert(&tests);
+  reedkiln_assert(&test_explicit_true);
+  reedkiln_assert(3.14159);
+  reedkiln_assert("a string");
+  reedkiln_assert('5');
+  reedkiln_assert(0.5);
   return Reedkiln_OK;
 }
 
